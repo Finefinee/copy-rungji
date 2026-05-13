e8d2d15e69e37a5c4c6586bd5546f06045ef8c33
김연호 <kimfine000@gmail.com>
2026-05-13 19:54:52 +0900

    안 쓰는 변수 제거

--- src/component/basic/Body.js
+++ src/component/basic/Body.js
@@ -1,16 +1,16 @@
-import {Children, useState} from "react";
+// import {useState} from "react";
 
 function Body({children}) {
 
-  const [number, setNumber] = useState(0);
+  // const [number, setNumber] = useState(0);
 
-  const handleClickNumber = () => {
-    setNumber(number + 1);
-  }
+  // const handleClickNumber = () => {
+  //   setNumber(number + 1);
+  // }
 
-  const num1 = 0;
-  const str1 = "??";
-  const bool1 = true; // 이건 표현 안된다
+  // const num1 = 0;
+  // const str1 = "??";
+  // const bool1 = true; // 이건 표현 안된다
 
   return (
     <div>
