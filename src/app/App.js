2ebca8698a562497a202724d9b77c5fb8fd8afbe
김연호 <kimfine000@gmail.com>
2026-05-13 20:06:26 +0900

    쓸모 없는 코드 제거

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,5 @@
 import './App.css';
 import RungjiList from "../component/rungji/RungjiList";
-import Body from "../component/basic/Body";
-
-function ChildComp() {
-  return <div>자식 컴포넌트</div>
-}
 
 function App() {
 
